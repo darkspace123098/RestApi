655e7c2eaa6f589b51fe0fc58b4c336726c9d28a
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-05-04 08:11:39 +0000

    rest

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ const app = express()
 const PORT = 5000
 
 //Routes
+
+//Rest Api
 app.get("/api/users", (req,res) =>{
     return res.json(users);
 });
 
-app.listen(PORT,() => console.log(`Server started at ${PORT}`))
+
+app.get("/users", (req,res) =>{
+    const HTML = `
+    <ol>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    `;
+    res.send(HTML);
+});
+
+app.get("/api/users/:id", (req,res) =>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id===id);
+    return res.json(user);
+});
+
+app.listen(PORT,() => console.log(`Server started at ${PORT}`));
