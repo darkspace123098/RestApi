52d797f64ee7b9e99b08c7b38fce64af90835e42
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-05-04 07:53:13 +0000

    api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const app = express()
+const users = require("./MOCK_DATA.json")
 
+const app = express()
 const PORT = 5000
 
 //Routes
+app.get("/api/users", (req,res) =>{
+    return res.json(users);
+});
 
 app.listen(PORT,() => console.log(`Server started at ${PORT}`))
